f658125be37e2f8932141a6c1c84e44331725872
tomas249 <tomastvmv@gmail.com>
2022-10-13 21:39:23 +0200

    add types and groups selection

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ type Event = {
 	type?: string;
 	group?: string;
 	dedicatedTime?: number;
+	order: number;
 }
 
 type Day = {
@@ -27,7 +28,8 @@ const eventsDB: Event[] = [
 		title: "First event",
 		type: "task",
 		group: "Captura i preparacio de dades",
-		dedicatedTime: 156
+		dedicatedTime: 156,
+		order: 0
 	},
 	{
 		id: "2222",
@@ -35,7 +37,8 @@ const eventsDB: Event[] = [
 		title: "Second event",
 		type: "pac",
 		group: "Minera de dades",
-		dedicatedTime: 20
+		dedicatedTime: 20,
+		order: 1
 	},
 	{
 		id: "3333",
@@ -43,7 +46,8 @@ const eventsDB: Event[] = [
 		title: "Third event",
 		type: "task",
 		group: "mates",
-		dedicatedTime: 256
+		dedicatedTime: 256,
+		order: 0
 	}
 ]
 
@@ -60,7 +64,8 @@ const emptyEvent = {
 	group: '',
 	dedicatedTime: 0,
 	isNew: true,
-	isModified: false
+	isModified: false,
+	order: -1
 }
 
 const EVENTS = 'events'
@@ -69,7 +74,11 @@ const GROUPS = 'groups'
 
 function App() {
 	const [events, setEvents] = useState<EventsGroupedByDate>({})
-	const [selectedDate, setSelectedDate] = useState(2)
+	const [selectedDate, setSelectedDate] = useState<Day>({
+		day: 1,
+		date: "2022-10-1",
+		events: []
+	})
 	const [selectedEvent, setSelectedEvent] = useState<Event & { isNew?: boolean, isModified?: boolean }>(emptyEvent)
 
 	// Load initial events
@@ -94,8 +103,7 @@ function App() {
 		}
 	}
 
-	function onChangeInput(event: React.ChangeEvent<HTMLInputElement>) {
-		const {name, value} = event.target
+	function changeSelectedEvent(name: string, value: unknown) {
 		setSelectedEvent(prev => ({
 			...prev,
 			[name]: value,
@@ -103,25 +111,35 @@ function App() {
 		}))
 	}
 
+	function onChangeInput(event: React.ChangeEvent<HTMLInputElement>) {
+		const {name, value} = event.target
+		changeSelectedEvent(name, value)
+	}
+
 	return (
 		<ContainerStyled>
 			<GridContainerStyled>
 				{generateCalendarCells().map((cell, index) => (
 					<ItemStyled key={index} onClick={(event) => {
-						// if (cell.day !== selectedDate) {
-						setSelectedDate(cell.day)
+						setSelectedDate(cell)
 						if (event.target === event.currentTarget && !selectedEvent.isNew) {
 							setSelectedEvent(emptyEvent)
 						}
-						// }
 					}}>
-						<div className="header">{cell.day}{cell.day === selectedDate ? '*' : ''}</div>
-						{/* cell.events << populate */}
+						<div className="header">{cell.day}{cell.date === selectedDate.date ? '*' : ''}</div>
 						{events[cell.date]?.map(event => (
 							<EventStyled key={event.id}
 							             onClick={() => setSelectedEvent(event.id === selectedEvent.id ? emptyEvent : event)}>
-								<span>{event.title}{event.id === selectedEvent.id ? '*' : ''}</span>
-								<button onClick={e => {
+								<div className="container">
+									<div style={{display: 'flex'}}>
+										<span className="info">{event.group}</span>
+										<span className="info">{event.dedicatedTime}</span>
+										{event.id === selectedEvent.id && <span style={{fontSize: '.8rem'}}>*</span>}
+										<span className="info" style={{marginLeft: 'auto'}}>{event.type}</span>
+									</div>
+									<span>{event.title}</span>
+								</div>
+								<button className="delete" onClick={e => {
 									e.stopPropagation()
 									setEvents(prevEvents => {
 										const newEvents = ({
@@ -143,60 +161,86 @@ function App() {
 				))}
 			</GridContainerStyled>
 			<div>
-				<div>
-					<h1>
-
-					</h1>
-				</div>
+				<AddEventContainerStyled>
+					<h3>day {selectedDate.day}</h3>
+					<input name="title" type="text" placeholder="title" value={selectedEvent.title} onChange={onChangeInput}/>
+					<input name="type" type="text" placeholder="type" value={selectedEvent.type}
+					       onChange={onChangeInput}/>
+					<input name="group" type="text" placeholder="group" value={selectedEvent.group}
+					       onChange={onChangeInput}/>
+					<input name="dedicatedTime" type="number" placeholder="dedicated time" value={selectedEvent.dedicatedTime}
+					       onChange={onChangeInput}/>
+					<input name="order" type="number" placeholder="order"
+					       value={selectedEvent.order === -1 ? events[selectedDate.day]?.length : selectedEvent.order}
+					       onChange={onChangeInput}/>
+					{/*	Actions*/}
+					{selectedEvent.isNew ? (
+						<>
+							<button onClick={() => {
+								const newEvent = {
+									title: selectedEvent.title,
+									type: selectedEvent.type,
+									group: selectedEvent.group,
+									dedicatedTime: selectedEvent.dedicatedTime,
+									id: generateUID(),
+									// date: `2022-10-${addZeroIfNeeded(selectedDate.day)}`,
+									date: selectedDate.date,
+								}
+								setEvents(prevEvents => {
+									console.log({newEvent})
+									const newEvents = ({
+										...prevEvents,
+										[newEvent.date]: (prevEvents[newEvent.date] || []).concat({...newEvent, order: 999}),
+									})
+									saveLocally(newEvents)
+									return newEvents
+								})
+								setSelectedEvent({...newEvent, order: 999})
+							}}>Add event
+							</button>
+						</>
+					) : (
+						<>
+							<button onClick={() => {
+								setEvents(prevEvents => {
+									const newEvents = ({
+										...prevEvents,
+										[selectedEvent.date]: prevEvents[selectedEvent.date].map(ev => ev.id === selectedEvent.id ? selectedEvent : ev)
+									})
+									saveLocally(newEvents)
+									return newEvents
+								})
+							}}>Modify event ({selectedEvent.isModified ? 'unsaved' : 'saved'})
+							</button>
+							<button onClick={() => setSelectedEvent(emptyEvent)}>Unselect event</button>
+						</>
+					)}
+				</AddEventContainerStyled>
+				<AddEventContainerStyled style={{marginTop: '1rem'}}>
+					<h3>Types</h3>
+					{[
+						'PAC',
+						'study'
+					].map((t, index) => (
+						<RounderPill key={index} selected={t === selectedEvent.type}
+						             onClick={() => changeSelectedEvent('type', t)}>{t}</RounderPill>
+					))}
+				</AddEventContainerStyled>
+				<AddEventContainerStyled style={{marginTop: '1rem'}}>
+					<h3>Groups</h3>
+					{[
+						'AU',
+						'CiP de dades',
+						'DiG de projectes',
+						'PiS de les dades',
+						'TEX',
+						'V de dades'
+					].map((g, index) => (
+						<RounderPill key={index} selected={g === selectedEvent.group}
+						             onClick={() => changeSelectedEvent('group', g)}>{g}</RounderPill>
+					))}
+				</AddEventContainerStyled>
 			</div>
-			<AddEventContainerStyled>
-				<h3>day {selectedDate}</h3>
-				<input name="title" type="text" placeholder="title" value={selectedEvent.title} onChange={onChangeInput}/>
-				<input name="type" type="text" placeholder="type" value={selectedEvent.type} onChange={onChangeInput}/>
-				<input name="group" type="text" placeholder="group" value={selectedEvent.group} onChange={onChangeInput}/>
-				<input name="dedicatedTime" type="number" placeholder="dedicated time" value={selectedEvent.dedicatedTime}
-				       onChange={onChangeInput}/>
-				{/*	Actions*/}
-				{selectedEvent.isNew ? (
-					<>
-						<button onClick={() => {
-							const newEvent = {
-								title: selectedEvent.title,
-								type: selectedEvent.type,
-								group: selectedEvent.group,
-								dedicatedTime: selectedEvent.dedicatedTime,
-								id: generateUID(),
-								date: `2022-10-${addZeroIfNeeded(selectedDate)}`,
-							}
-							setEvents(prevEvents => {
-								const newEvents = ({
-									...prevEvents,
-									[newEvent.date]: (prevEvents[newEvent.date] || []).concat(newEvent)
-								})
-								saveLocally(newEvents)
-								return newEvents
-							})
-							setSelectedEvent(newEvent)
-						}}>Add event
-						</button>
-					</>
-				) : (
-					<>
-						<button onClick={() => {
-							setEvents(prevEvents => {
-								const newEvents = ({
-									...prevEvents,
-									[selectedEvent.date]: prevEvents[selectedEvent.date].map(ev => ev.id === selectedEvent.id ? selectedEvent : ev)
-								})
-								saveLocally(newEvents)
-								return newEvents
-							})
-						}}>Modify event ({selectedEvent.isModified ? 'unsaved' : 'saved'})
-						</button>
-						<button onClick={() => setSelectedEvent(emptyEvent)}>Unselect event</button>
-					</>
-				)}
-			</AddEventContainerStyled>
 		</ContainerStyled>
 	);
 }
@@ -209,6 +253,7 @@ const ContainerStyled = styled.div`
 `
 
 const GridContainerStyled = styled.div`
+  height: min-content;
   display: grid;
   grid-template-columns: repeat(7, 150px);
   grid-auto-rows: minmax(120px, auto);
@@ -230,15 +275,62 @@ const ItemStyled = styled.div`
 
 const EventStyled = styled.div`
   display: flex;
+  position: relative;
   justify-content: space-between;
   border-radius: .4rem;
   background-color: aquamarine;
+  //border-top: 4px solid aquamarine;
+  //border-bottom: 4px solid aquamarine;
   padding: .2rem .1rem;
   margin-top: .2rem;
   cursor: pointer;
 
   :last-child {
     margin-bottom: .2rem;
+  }
+
+  .container {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+
+    .info {
+      margin-right: 0.2rem;
+      font-size: .6rem;
+      border-radius: .4rem;
+      background-color: #fda07e;
+      padding: .1rem .2rem;
+    }
+  }
+
+  :hover {
+    border-top-right-radius: 0;
+    border-bottom-right-radius: 0;
+    background-color: #208c68;
+
+    .delete {
+      display: block;
+    }
+  }
+
+  .delete {
+    z-index: 2;
+    cursor: pointer;
+    display: none;
+    position: absolute;
+    top: 0;
+    left: 100%;
+    font-size: .6rem;
+    border: none;
+    background-color: #208c68;
+    padding: 0 .6rem;
+    height: 100%;
+    border-top-right-radius: .4rem;
+    border-bottom-right-radius: .4rem;
+
+    :hover {
+      background-color: rgb(196, 176, 176);
+    }
   }
 
   // No select
@@ -262,6 +354,14 @@ const AddEventContainerStyled = styled.div`
   > * {
     margin-top: .8rem;
   }
+`
+
+const RounderPill = styled.div<{ selected?: boolean }>`
+  border-radius: .4rem;
+  background-color: ${({selected = false}) => selected ? '#008f5f' : 'aquamarine'};
+  padding: .2rem .4rem;
+  font-size: .8rem;
+  cursor: pointer;
 `
 
 const DAYS_IN_A_WEEK = 7
