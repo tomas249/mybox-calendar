606f4505fa087bd2bc5b5d8ac22609561f976844
tomas249 <tomastvmv@gmail.com>
2022-11-10 02:48:33 +0000

    add tailwind

--- src/components/Event.tsx
+++ src/components/Event.tsx
@@ -1,24 +1,15 @@
-import styled from "styled-components";
-
-type EventProps = {
-	title: string;
-	description?: string;
-	subject?: string;
-	timeDedicated?: number;
+export default function App() {
+  return (
+    <div className="flex">
+  <div className="flex-none w-14 h-14 bg-orange-400">
+    01
+  </div>
+  <div className="flex-initial w-64 ...">
+    02
+  </div>
+  <div className="flex-initial w-32 ...">
+    03
+  </div>
+</div>
+  )
 }
-
-const EventStyled = styled.div`
-  border-radius: .4rem;
-  background-color: aquamarine;
-  padding: .2rem .1rem;
-  margin-top: .2rem;
-  cursor: pointer;
-
-  // No select
-  -webkit-touch-callout: none; /* iOS Safari */
-  -webkit-user-select: none; /* Safari */
-  -khtml-user-select: none; /* Konqueror HTML */
-  -moz-user-select: none; /* Old versions of Firefox */
-  -ms-user-select: none; /* Internet Explorer/Edge */
-  user-select: none; /* Non-prefixed version, currently */
-`
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import {
+  createBrowserRouter,
+  RouterProvider
+} from "react-router-dom";
+
 import App from './App';
+import AppV2 from './components/Event'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppV2 />,
+  },
+]);
 
 const root = ReactDOM.createRoot(
 	document.getElementById('root') as HTMLElement
 );
 root.render(
 	<React.StrictMode>
-		<App/>
+		<RouterProvider router={router} />
+    {/* <App/> */}
 	</React.StrictMode>
 );
 
